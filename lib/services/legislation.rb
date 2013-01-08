6b1edc840778a4fad2b7e5af35765d7dbe61f5ab
John Perry <jgperry@ajc.com>
2013-01-08 17:11:46 -0500

    added #get_legislation_for_session and #get_legislation_detail

--- lib/services/legislation.rb
+++ lib/services/legislation.rb
@@ -1,2 +1,21 @@
-class Legislation
+module GGAServices
+	class Legislation
+		attr_accessor :summary_response, :detail_response
+		def initialize
+			@client = Savon.client(
+				wsdl: 'http://webservices.legis.ga.gov/GGAServices/Legislation/Service.svc?wsdl',
+				convert_request_keys_to: :camelcase
+			)
+		end
+
+		def get_legislation_for_session(session_id)
+			message = { session_id: session_id }
+			@summary_response = @client.call(:get_legislation_for_session, message: message)
+		end
+
+		def get_legislation_detail(legislation_id)
+			message = { legislation_id: legislation_id }
+			@detail_response = @client.call(:get_legislation_detail, message: message)
+		end
+	end
 end
--- spec/legislation_spec.rb
+++ spec/legislation_spec.rb
@@ -1,8 +1,30 @@
 require 'spec_helper'
 require 'savon/mock/spec_helper'
 
-describe Legislation do
+describe GGAServices::Legislation do
 	include Savon::SpecHelper
 	before(:all) { savon.mock! }
 	after(:all)  { savon.unmock! }
+
+	describe "#get_legislation_for_session" do
+		it "should return vote list" do
+			fixture = File.read('spec/fixtures/legislation_get_legislation_for_session.xml')
+			message = { session_id: 21 }
+			savon.expects(:get_legislation_for_session).with(message: message).returns(fixture)
+
+			votes = GGAServices::Legislation.new
+			expect( votes.get_legislation_for_session(21) ).to be_successful
+		end
+	end
+
+	describe "#get_legislation_detail" do
+		it "should return vote list" do
+			fixture = File.read('spec/fixtures/legislation_get_legislation_detail.xml')
+			message = { legislation_id: 33209 }
+			savon.expects(:get_legislation_detail).with(message: message).returns(fixture)
+
+			votes = GGAServices::Legislation.new
+			expect( votes.get_legislation_detail(33209) ).to be_successful
+		end
+	end
 end
