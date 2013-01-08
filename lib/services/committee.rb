d7af579444c32c43c482ef41becd8bf4d9a931bd
John Perry <jgperry@ajc.com>
2013-01-08 14:37:27 -0500

    added #get_committees_by_session and #get_committees_by_type_and_session methods

--- lib/services/committee.rb
+++ lib/services/committee.rb
@@ -1,2 +1,20 @@
-class Committee
+module GGAServices
+	class Committee
+		def initialize
+			@client = Savon.client(
+				wsdl: 'http://webservices.legis.ga.gov/GGAServices/Committees/Service.svc?wsdl',
+				convert_request_keys_to: :camelcase
+			)
+		end
+
+		def get_committees_by_session(session_id)
+			message = { session_id: session_id }
+			@client.call(:get_committees_by_session, message: message)
+		end
+
+		def get_committees_by_type_and_session(type, session_id)
+			message = { type: type, session_id: session_id }
+			@client.call(:get_committees_by_type_and_session, message: message)
+		end
+	end
 end
--- spec/committee_spec.rb
+++ spec/committee_spec.rb
@@ -1,8 +1,30 @@
 require 'spec_helper'
 require 'savon/mock/spec_helper'
 
-describe Committee do
+describe GGAServices::Committee do
 	include Savon::SpecHelper
 	before(:all) { savon.mock! }
 	after(:all)  { savon.unmock! }
+
+	describe "#get_committees_by_session" do
+		it "should return committee list" do
+			fixture = File.read('spec/fixtures/committees_get_committees_by_session.xml')
+			message = { session_id: 22 }
+			savon.expects(:get_committees_by_session).with(message: message).returns(fixture)
+
+			committees = GGAServices::Committee.new
+			expect( committees.get_committees_by_session(22) ).to be_successful
+		end
+	end
+
+	describe "#get_committees_by_type_and_session" do
+		it "should return committee list" do
+			fixture = File.read('spec/fixtures/committees_get_committees_by_type_and_session.xml')
+			message = { type: 'House', session_id: 22 }
+			savon.expects(:get_committees_by_type_and_session).with(message: message).returns(fixture)
+
+			committees = GGAServices::Committee.new
+			expect( committees.get_committees_by_type_and_session('House', 22) ).to be_successful
+		end
+	end
 end
