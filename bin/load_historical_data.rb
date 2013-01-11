0e707e9053c375fbbebd231b909db2892ddba9b2
John Perry <jgperry@gmail.com>
2013-01-11 10:52:16 -0500

    added example code

---
+++ bin/load_historical_data.rb
@@ -0,0 +1,29 @@
+require 'rubygems'
+require 'dbi'
+require_relative '../lib/gga_services'
+
+include GGAServices
+
+connect_string = 'DBI:Mysql:gga_services:XXXX'
+@dbh = DBI.connect( connect_string,
+                    "XXXX",
+                    "XXXX" )
+sql_format = 'INSERT IGNORE INTO sessions VALUES("%s","%s","%s","%s"")'
+
+sessions_service = LegislativeSession.new
+sessions = sessions_service.get_sessions.body[:get_sessions_response][:get_sessions_result][:session]
+
+count = 0
+sessions.each do |session|
+  sql = sql_format % [ session[:is_default], session[:id], session[:description], session[:library] ]
+  
+  begin
+    count += @dbh.do( sql )
+  rescue DBI::DatabaseError => e
+    print e.errstr
+  rescue => e
+    print e.message
+  end
+end
+
+print "#{count} records inserted"
