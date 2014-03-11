fec1de2fb3b5a1700c8b2908b84bad17c9dfb2d3
John Perry <jgperry@ajc.com>
2014-03-11 10:55:58 -0400

    legislative day calendar scraper

---
+++ bin/legislative_day_scraper.rb
@@ -0,0 +1,69 @@
+require 'open-uri'
+require 'nokogiri'
+require 'active_record'
+require 'mysql2'
+require 'logger'
+
+
+LOG = Logger.new('logs/days_left_log.txt', 'monthly')
+# LOG = Logger.new(STDOUT)
+LOG.level = Logger::INFO
+
+ActiveRecord::Base.establish_connection(
+  adapter: "mysql2",
+  host: ENV["GGA_HOST"],
+  username: ENV["GGA_USER"],
+  password: ENV["GGA_PASSWORD"],
+  database: ENV["GGA_DATABASE"]
+)
+
+class LegislativeDay < ActiveRecord::Base
+  self.table_name = "gga_staging.legislative_days"
+  self.primary_key = "id"
+end
+
+begin
+  page = Nokogiri::HTML(open('http://www.house.ga.gov/clerk/en-US/HouseCalendars.aspx'))
+rescue => error
+  LOG.fatal error
+  fail
+end
+
+items = page.xpath('//select')
+
+if items.length == 0
+  LOG.fatal 'No select elements found on page'
+  fail
+end
+
+items.each do |select|
+  next unless  select["id"].match(/Legislative Day/)
+
+  if select.children.length == 0
+    LOG.fatal "No select options found in select element"
+    fail
+  end
+
+  select.children.each do |option|
+    next if option["value"].match(/2013/)
+    day = Hash.new
+    day[:legislative_day_date] = option["value"]
+
+    option.children[0].content.match(/Day (\d+)/)
+    day[:id] = $1.to_i
+
+    begin
+      LegislativeDay.find_or_create_by(id: day[:id]).update(day)
+    rescue => error
+      LOG.error error
+    end
+  end
+end
+
+today = Time.now.strftime('%Y-%m-%d')
+legislative_day = LegislativeDay.find_by legislative_day_date: today
+LOG.info "#{40 - legislative_day.id} days left in session"
+
+
+
+
--- gga_daily_update.sh
+++ gga_daily_update.sh
@@ -4,4 +4,5 @@ env > /home/john/environment.txt
 rvm 2.1.0@gga
 ruby ~/gga-services/bin/load_session_bills.rb
 ruby ~/gga-services/bin/load_votes.rb
+ruby ~/gga-services/bin/legislative_day_scraper.rb
 
